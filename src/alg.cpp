c6179c9a8239e15512c19febb7c306b225e485ab
Irina <116427830+11Rin@users.noreply.github.com>
2023-02-26 19:20:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,7 +28,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    long int sum = 0;
+    int sum = 0;
     for (int j = 2; j < hbound; j++) {
         if (checkPrime(j)) sum = sum + j;
     }
